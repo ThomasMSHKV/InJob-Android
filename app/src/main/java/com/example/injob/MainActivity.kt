91e3b2b682c5c6370e84e792a42586d869a002fa
OlegDeveloper <tmereshkov@gmail.com>
2022-11-05 16:09:54 +0300

    bottomNavigation

--- app/src/main/java/com/example/injob/MainActivity.kt
+++ app/src/main/java/com/example/injob/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.injob
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.injob.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    private val mBinding get() = binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2500)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(mBinding.root)
+        }
     }
 }
