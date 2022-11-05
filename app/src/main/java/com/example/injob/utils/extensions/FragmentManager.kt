0efbe004bc789d9153f46ed15698c14b14129b46
OlegDeveloper <tmereshkov@gmail.com>
2022-11-05 12:12:27 +0300

    FragmentManager.kt

---
+++ app/src/main/java/com/example/injob/utils/extensions/FragmentManager.kt
@@ -0,0 +1,19 @@
+package com.example.injob.utils.extensions
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.injob.R
+
+
+fun FragmentManager.navigateToFragment(fragment: Fragment, addToBackStack: Boolean = true) {
+    if (addToBackStack) {
+        beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .addToBackStack(null)
+            .commit()
+    } else {
+        beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
